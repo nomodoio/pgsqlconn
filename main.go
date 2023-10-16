d9af4bac9db2ebd66e00bff3f437dba2477b17c4
Josef Podany <podany.pepa@gmail.com>
2023-10-16 14:27:31 +0200

    postgresql connection test

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	host     = ""
+	port     = 5432
+	user     = ""
+	password = ""
+	dbname   = ""
+)
 
 func main() {
 	fmt.Println("nomodo.io postgresql connection test")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("successfully connected!")
 }
